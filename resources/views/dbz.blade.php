61232ed16797fb8a4164c059b18cbfa5fdf68e37
ViniciusPolo <admviniciuspolo@gmail.com>
2022-01-25 23:03:01 -0300

    exercicio2

--- resources/views/dbz.blade.php
+++ resources/views/dbz.blade.php
@@ -8,9 +8,9 @@
 </head>
 <body>
     @if ($energy > 8000) 
-        <h1>Mais que 8000!</h1>
+        <h1>Mais de 8000!</h1>
     @else 
-        <h1>Inseto</h1>
+        <h1>Inseto!</h1>
     @endif
 </body>
 </html>
